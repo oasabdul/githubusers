707c63224376d73435082fc13de6902e4d116a39
Abdul Rahman <intija234@gmail.com>
2024-11-30 14:22:15 +0100

    Remove placeholder data

--- data/profile/src/main/java/com/moneyfwd/profile/repository/ProfileRepositoryImpl.kt
+++ data/profile/src/main/java/com/moneyfwd/profile/repository/ProfileRepositoryImpl.kt
@@ -1,7 +1,5 @@
 package com.moneyfwd.profile.repository
 
-import com.moneyfwd.domain.profile.model.ProfileDetails
-import com.moneyfwd.domain.profile.model.RepositoryItem
 import com.moneyfwd.domain.profile.model.Username
 import com.moneyfwd.domain.profile.repository.GetUserProfileResponse
 import com.moneyfwd.domain.profile.repository.GetUserRepositoriesResponse
@@ -11,7 +9,6 @@ import retrofit2.HttpException
 
 class ProfileRepositoryImpl(private val apiService: ProfileApiService) : ProfileRepository {
     override suspend fun getUserRepositories(username: Username): GetUserRepositoriesResponse {
-        return userRepositoriesDummy()
         try {
             val response = apiService.getUserRepositories(username)
 
@@ -27,7 +24,6 @@ class ProfileRepositoryImpl(private val apiService: ProfileApiService) : Profile
     }
 
     override suspend fun getUserProfile(username: Username): GetUserProfileResponse {
-        return userProfileDummy()
         try {
             val profileResponse = apiService.getUserProfile(username)
             return GetUserProfileResponse.Success(userProfileDetails = profileResponse.mapToDomainModel())
@@ -37,22 +33,4 @@ class ProfileRepositoryImpl(private val apiService: ProfileApiService) : Profile
             return GetUserProfileResponse.UnknownError
         }
     }
-
-    private fun userRepositoriesDummy() = GetUserRepositoriesResponse.Success(
-        userRepositories = listOf(
-            RepositoryItem(name = "Sample repo name", language = "Java", stars = 105, description = "Shows an example repo based on assignment given", htmlUrl = "https://github.com/login"),
-            RepositoryItem(name = "Sample repo name", language = "Java", stars = 105, description = "Shows an example repo based on assignment given", htmlUrl = "https://github.com/login"),
-            RepositoryItem(name = "Sample repo name", language = "Java", stars = 105, description = "Shows an example repo based on assignment given", htmlUrl = "https://github.com/login"),
-            RepositoryItem(name = "Sample repo name", language = "Java", stars = 105, description = "Shows an example repo based on assignment given", htmlUrl = "https://github.com/login"),
-            RepositoryItem(name = "Sample repo name", language = "Java", stars = 105, description = "Shows an example repo based on assignment given", htmlUrl = "https://github.com/login"),
-            RepositoryItem(name = "Sample repo name", language = "Java", stars = 105, description = "Shows an example repo based on assignment given", htmlUrl = "https://github.com/login"),
-            RepositoryItem(name = "Sample repo name", language = "Java", stars = 105, description = "Shows an example repo based on assignment given", htmlUrl = "https://github.com/login"),
-            RepositoryItem(name = "Sample repo name", language = "Java", stars = 105, description = "Shows an example repo based on assignment given", htmlUrl = "https://github.com/login"),
-            RepositoryItem(name = "Sample repo name", language = "Java", stars = 105, description = "Shows an example repo based on assignment given", htmlUrl = "https://github.com/login"),
-        )
-    )
-
-    private fun userProfileDummy() = GetUserProfileResponse.Success(
-        userProfileDetails = ProfileDetails(profilePictureUrl = "https://i.pinimg.com/originals/76/80/4f/76804f67ba38f85e4802d250e5b15504.jpg", username = "abdulIntija", fullName = "Mark Pain", followerCount = 236, followingCount = 32)
-    )
 }
--- data/search/src/main/java/com/moneyfwd/data/search/repository/SearchUserRepositoryImpl.kt
+++ data/search/src/main/java/com/moneyfwd/data/search/repository/SearchUserRepositoryImpl.kt
@@ -2,7 +2,6 @@ package com.moneyfwd.data.search.repository
 
 import com.moneyfwd.data.search.network.SearchApiService
 import com.moneyfwd.domain.search.model.SearchQuery
-import com.moneyfwd.domain.search.model.SearchUserItem
 import com.moneyfwd.domain.search.repository.SearchUserRepositoryResponse
 import com.moneyfwd.domain.search.repository.SearchUserRepositoryResponse.NetworkError
 import com.moneyfwd.domain.search.repository.SearchUserRepositoryResponse.UnknownError
@@ -12,7 +11,6 @@ import retrofit2.HttpException
 
 class SearchUserRepositoryImpl(private val apiService: SearchApiService): SearchUsersRepository {
     override suspend fun searchUsers(searchQuery: SearchQuery): SearchUserRepositoryResponse {
-        return searchDummy()
         try {
             val request = apiService.searchUsers(searchQuery.query)
             return Success(searchResults = request.items.map { it.mapToDomainModel() })
@@ -22,20 +20,4 @@ class SearchUserRepositoryImpl(private val apiService: SearchApiService): Search
             return UnknownError
         }
     }
-
-    private fun searchDummy() = SearchUserRepositoryResponse.Success(
-        searchResults = listOf(
-            SearchUserItem(name = "abdulIntija", thumbImgUrl = "https://i.pinimg.com/originals/76/80/4f/76804f67ba38f85e4802d250e5b15504.jpg"),
-            SearchUserItem(name = "abdulIntija", thumbImgUrl = "https://i.pinimg.com/originals/76/80/4f/76804f67ba38f85e4802d250e5b15504.jpg"),
-            SearchUserItem(name = "abdulIntija", thumbImgUrl = "https://i.pinimg.com/originals/76/80/4f/76804f67ba38f85e4802d250e5b15504.jpg"),
-            SearchUserItem(name = "abdulIntija", thumbImgUrl = "https://i.pinimg.com/originals/76/80/4f/76804f67ba38f85e4802d250e5b15504.jpg"),
-            SearchUserItem(name = "abdulIntija", thumbImgUrl = "https://i.pinimg.com/originals/76/80/4f/76804f67ba38f85e4802d250e5b15504.jpg"),
-            SearchUserItem(name = "abdulIntija", thumbImgUrl = "https://i.pinimg.com/originals/76/80/4f/76804f67ba38f85e4802d250e5b15504.jpg"),
-            SearchUserItem(name = "abdulIntija", thumbImgUrl = "https://i.pinimg.com/originals/76/80/4f/76804f67ba38f85e4802d250e5b15504.jpg"),
-            SearchUserItem(name = "abdulIntija", thumbImgUrl = "https://i.pinimg.com/originals/76/80/4f/76804f67ba38f85e4802d250e5b15504.jpg"),
-            SearchUserItem(name = "abdulIntija", thumbImgUrl = "https://i.pinimg.com/originals/76/80/4f/76804f67ba38f85e4802d250e5b15504.jpg"),
-            SearchUserItem(name = "abdulIntija", thumbImgUrl = "https://i.pinimg.com/originals/76/80/4f/76804f67ba38f85e4802d250e5b15504.jpg"),
-            SearchUserItem(name = "abdulIntija", thumbImgUrl = "https://i.pinimg.com/originals/76/80/4f/76804f67ba38f85e4802d250e5b15504.jpg"),
-        )
-    )
 }
